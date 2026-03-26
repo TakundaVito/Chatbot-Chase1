55d5dd374208e215fb04fb24a8e7a20da6c1a3cc
TakundaVito <35930530+TakundaVito@users.noreply.github.com>
2026-03-26 18:18:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const https = require("https");
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());

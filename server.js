0ca9cafd44b372dcf8b7ad82e23a189f41efbab1
TakundaVito <35930530+TakundaVito@users.noreply.github.com>
2026-04-03 16:48:40 +0200

    Update server.js
    admin 2

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const agent = new https.Agent({ family: 4 });
 // 🤖 BOT MODE & COEXISTENCE
 // ===============================
 let botMode = true;
-let adminNumber = "+263 77 741 6664";          // General admin for courier/tracking
-let bikeAdminNumber = "+263 77 583 7909 ";      // Tadiwanashe Marufu – bike deliveries
-let taxiAdminNumber = "+263 77 581 3837";      // Taxi to Gweru
+let adminNumber = "+263777416664";          // General admin for courier/tracking
+let bikeAdminNumber = "+263775837909 ";      // Tadiwanashe Marufu – bike deliveries
+let taxiAdminNumber = "+263775837909";      // Taxi to Gweru
 
 const SESSION_TIMEOUT = 10 * 60 * 1000;
 let userLastActivity = {};
